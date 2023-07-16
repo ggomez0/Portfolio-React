fb24864e1f846a1507002e10673c2a675543f6d7
ggomez0 <gaspargomez2000@outlook.com>
2023-07-16 20:48:16 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (<div>
+    <div>
+    <h1>GASPAR GOMEZ</h1>
+    </div>
+    <div>
+      <button variant="contained"> Github</button>
+    </div>
+  </div>)
+}
+
+export default App
